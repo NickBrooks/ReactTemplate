9938bca49cebf90acaeb2160997ef953d3ae8b0f
Nick Brooks <nbrooks@thirtyseven.com.au>
2017-05-12 13:21:46 +0930

    Initial template

---
+++ client/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import Style from './styles/index.scss';
+
+const app = (
+    <p>Hello</p>
+)
+
+render(app, document.getElementById('root'));
